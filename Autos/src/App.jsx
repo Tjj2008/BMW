2d16954e7b911acef90e593753a1580f8cf7aa80
Aylen2005 <vabusto@escuelasproa.edu.ar>
2023-11-03 14:49:22 -0300

    whatttt

--- Autos/src/App.jsx
+++ Autos/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Card from './card'; // Asegúrate de que la ruta sea correcta
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    
+    <div className='card-container'>
+      <Card
+        imageUrl="https://www.peugeot.com.ar/content/dam/peugeot/argentina/b2c/our-range/bbc/3008-Baseball-card-new.png"
+        title="Peugeot 3008"
+        
+      />
 
-export default App
+<Card
+        imageUrl="https://interamericananorte.com/storage/cars/Ldc3L3v1hT6J8L26niVq5CSTdLu4J5JkzgZVZpwW.png"
+        title="Peugeot 2008"
+        
+      />
+
+<Card
+        imageUrl="https://gsegno2.files.wordpress.com/2020/09/peugeot-5008-2021.png"
+        title="Peugeot 5008"
+        
+      />
+
+<Card
+        imageUrl="https://www.podersa.com.ar/plantilla/assets/vehiculos2/20801.jpg"
+        title="Peugeot 208"
+        
+      />
+
+<Card
+        imageUrl="https://images.carexpert.com.au/app/uploads/2022/08/2023-peugeot-308-7-etched-1660276631-s-2172x1116.png"
+        title="Peugeot 308"
+        
+      />
+
+<Card
+        imageUrl="https://autodesignmagazine.com/wp-content/uploads/2022/06/2022062701_Peugeot408.png"
+        title="Peugeot 408"
+        
+      />
+
+<Card
+        imageUrl="https://static.rajamobil.com/media/images/databasemobil/mobilbaru/color/1677221757-Alphine_White_Uni.png"
+        title="BMW x5"
+        
+      />
+
+<Card
+        imageUrl="https://prod.cosy.bmw.cloud/bmwweb/cosySec?COSY-EU-100-7331cqgv2Z7d%25i02uCaY3MuO2kOHUtWPfbYf6JDc10tLhu1XzWVo7puMLWFmdkAj5DOP7fO%25Z8XgY1nTNIowJ4HO3zkyXq%25sGM8snpq6v6ODubLz2aKqfUK8jmB2fJj5DOP5Eagd%25kcWExHWpbl8FO2k3Hy2o242E%25TQBrXpFpNzlZ24riIifHscpF4HvHjb0KiIFJG8IXABHvIT9PMwO2JGvloMdFgpT9GsLDbjUilo90yYw6bHsLoACeOchJ0yLOEjgmqTACygN6C8mlOECUkwBP7sgNEbn%25Yp10UkNh5utTVAbnkq83h7zOh5nmPXB7agq857MruZRUmP81D5PAxb7MPVY8teWh1DMztPloeqVYDafM3ZjmztYRSDXR67aftxdYrBw1RSfWQthY%25VxdSeZ4FjuzWQdjcFI13aeZQ6KIvNXRjcZwBvAHrx6Kc%252wGz4WwBKup%258QFe%252B3iuPcIjup2XH3ccv63iprJXMGGwXHi4TrbI9%25rJHFlHEOou4TJIsJRgL3FlTv0TfryXIslGAlvJCrv0s9OsN3E4GA0ogKLNNF1AMd8U0%25lctO1qTMJFsyZLB9Weu0"
+        title="BMW x4"
+        
+      />
+
+
+<Card
+        imageUrl="https://bmw-laxmi.com.np/assets/frontend/images/all-models/x-series/x3/inform/cosySec7.png"
+        title="BMW x3"
+        
+      />
+    </div>
+    
+  );
+};
+
+export default App;
